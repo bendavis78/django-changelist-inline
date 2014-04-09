4802b56cfae9dc5e796982208a3efce55c9ab5f0
Ben Davis <bendavis78@gmail.com>
2014-04-09 16:30:41 -0500

    Added support for generic inlines

--- changelist_inline/admin.py
+++ changelist_inline/admin.py
@@ -1,12 +1,16 @@
 import urllib
+from collections import namedtuple
 
 from django import forms
-from django.forms.models import _get_foreign_key
 from django.contrib import admin
 from django.contrib.admin.options import InlineModelAdmin
 from django.contrib.admin.templatetags.admin_static import static
+from django.contrib.contenttypes.forms import BaseGenericInlineFormSet
+from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.template.loader import select_template
+
+GenericFK = namedtuple('GenericFK', ['ct_field', 'fk_field'])
 
 
 class ChangeListInline(InlineModelAdmin):
@@ -22,11 +26,7 @@ class ChangeListInline(InlineModelAdmin):
             # don't continue if we're the hybrid modeladmin we've created below
             return
 
-        self.context = None
         self.parent_obj = None
-        self.fk = _get_foreign_key(parent_model, self.model,
-                                   fk_name=self.fk_name)
-
         # Create a Modeldmin to be used to render the change list
         modeladmin_name = '{0}ModelAdmin'.format(self.__class__.__name__)
         modeladmin_attrs = {}
@@ -42,10 +42,19 @@ class ChangeListInline(InlineModelAdmin):
                           modeladmin_attrs)
         self._modeladmin = ModelAdmin(self.model, admin_site)
 
+    def get_fk_field(self, request, obj, **kwargs):
+        if not hasattr(self, '_fk'):
+            formset = self.get_formset(request, obj, **kwargs)
+            if issubclass(formset, BaseGenericInlineFormSet):
+                self._fk = GenericFK(formset.ct_field, formset.ct_fk_field)
+            else:
+                self._fk = formset.fk
+        return self._fk
+
     def get_fieldsets(self, request, obj=None, **kwargs):
-        self.parent_obj = obj
         if not obj:
             return []
+        self.parent_obj = obj
 
         # Allows us to do stuff with request before we're put inside the
         # AdminInlineFormSet helper class
@@ -58,12 +67,22 @@ class ChangeListInline(InlineModelAdmin):
         # There's no "add another" in this inline.
         return 0
 
-    def get_add_url(self, obj, **kwargs):
+    def get_add_url(self, request, obj, **kwargs):
         urlname = 'admin:{0.app_label}_{0.model_name}_add'
         url = reverse(urlname.format(self.model._meta))
-        params = {
-            self.fk.name: getattr(obj, self.fk.rel.field_name)
-        }
+        fk = self.get_fk_field(request, obj, **kwargs)
+        if isinstance(fk, GenericFK):
+            # generic fk
+            ct_field, fk_field = fk
+            parent_type = ContentType.objects.get_for_model(self.parent_obj)
+            params = {
+                ct_field.name: parent_type.pk,
+                fk_field.name: self.parent_obj.pk
+            }
+        else:
+            params = {
+                fk.name: getattr(obj, fk.rel.field_name)
+            }
         params.update(kwargs)
         return '?'.join((url, urllib.urlencode(params)))
 
@@ -71,15 +90,27 @@ class ChangeListInline(InlineModelAdmin):
         qs = super(ChangeListInline, self).get_queryset(request)
         if not self.parent_obj:
             return qs.none()
-        return qs.filter(**{self.fk.name: self.parent_obj})
+        parent_type = ContentType.objects.get_for_model(self.parent_obj)
+        fk = self.get_fk_field(request, None)
+        if isinstance(fk, GenericFK):
+            # generic fk
+            ct_field, fk_field = fk
+            filter = {
+                ct_field.name: parent_type,
+                fk_field.name: self.parent_obj.pk
+            }
+            return qs.filter(**filter)
+        else:
+            # regular fk
+            return qs.filter(**{fk.name: self.parent_obj})
 
     def get_context(self, request, obj=None, extra_context=None):
-        # hijack the context from teh changelist_view template response
+        # hijack the context from the changelist_view template response
         response = self._modeladmin.changelist_view(request, extra_context)
         context = response.context_data
         context.update({
             'classes': ' '.join(self.classes),
-            'add_url': self.get_add_url(obj)
+            'add_url': self.get_add_url(request, obj)
         })
         return context
 
